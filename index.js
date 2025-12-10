1631818e1bd9b56b6fc5a0e106777b7042495908
Aleksandr Borkov <sbor767@gmail.com>
2025-12-09 23:39:46 -0800

    Add user router with login and register endpoints

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import { router as userRouter } from './users/users.js';
 
 const port = 8000;
 const app = express();
 
 app.get('/hello', (req, res) => {
-  res.send('Hello, World!');
+	res.send('Hello, World!');
 });
 
+app.use('/users', userRouter);
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+	console.log(`Server running at http://localhost:${port}/`);
 });
---
+++ users/users.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/login', (req, res) => {
+  res.send('/login');
+});
+
+router.post('/register', (req, res) => {
+  res.send('/register');
+});
+
+export { router };
