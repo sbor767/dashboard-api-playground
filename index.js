2f7da29b7a57a55af8b7a9f54521bc39f463a887
Aleksandr Borkov <sbor767@gmail.com>
2025-12-09 22:18:00 -0800

    Implement basic HTTP server in index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import http from 'http';
+
+const host = 'localhost';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
