0a5fc8e674e584c89292745b65149c7ce220c5f9
Aleksandr Borkov <sbor767@gmail.com>
2025-12-13 20:20:52 -0800

    feat: implement LoggerService for enhanced logging functionality

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Express } from 'express';
 import { router as userRouter } from './users/users.js';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.service.js';
 
 export class App {
 	app: Express;
 	server: Server;
 	port: number;
+	logger: LoggerService
 
-	constructor (){
+	constructor (logger: LoggerService) {
 		this.app = express();
 		this.port = 8000;
+		this.logger = logger
 	}
 
 	useRoutes()	{
@@ -19,6 +22,6 @@ export class App {
 	public async init() {
 		this.useRoutes();
 		this.server = this.app.listen(this.port);
-		console.log(`Server started on http://localhost:${this.port}`);
+		this.logger.log(`Server started on http://localhost:${this.port}`);
 	}			
 }
