a994ba432d130a68d838cbc567c57d61ec4762ed
Aleksandr Borkov <sbor767@gmail.com>
2025-12-14 20:38:36 -0800

    feat: refactor dependency injection setup using ContainerModule for better organization

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Container } from "inversify";
+import { Container, ContainerModule, interfaces } from "inversify";
 import { TYPES } from "./types";
 import { App } from "./app";
 import type { LoggerInteface } from "./logger/logger.interface";
@@ -7,12 +7,20 @@ import { LoggerService } from "./logger/logger.service";
 import { ExceptionFilter } from "./errors/exception.filter";
 import { UserController } from "./users/users.controller";
 
-const appContainer = new Container();
-appContainer.bind<LoggerInteface>(TYPES.LoggerInterface).to(LoggerService);
-appContainer.bind<ExceptionFilterInterface>(TYPES.ExceptionFilter).to(ExceptionFilter);
-appContainer.bind<UserController>(TYPES.UserController).to(UserController);
-appContainer.bind<App>(TYPES.Application).to(App);
-const app = appContainer.get<App>(TYPES.Application);
-app.init();
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+	bind<LoggerInteface>(TYPES.LoggerInterface).to(LoggerService);
+	bind<ExceptionFilterInterface>(TYPES.ExceptionFilter).to(ExceptionFilter);
+	bind<UserController>(TYPES.UserController).to(UserController);
+	bind<App>(TYPES.Application).to(App);
+});
 
-export { app, appContainer }
+function bootstrap() {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
+	return { app, appContainer };
+} 
+
+
+export const { app, appContainer } = bootstrap();
